20832d2ba81b00df4fbf1ced4409e59ef81c352e
devMfawzy <m.fawzy.a@icloud.com>
2022-08-21 14:36:53 +0200

    Initial Commit

---
+++ ChargingStations/ChargingStationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChargingStationsApp.swift
+//  ChargingStations
+//
+//  Created by Mohamed Fawzy on 21/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct ChargingStationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
